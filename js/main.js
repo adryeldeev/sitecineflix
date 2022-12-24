9f3851673829fde047cb37516d0950f8cbef73de
Adryel martins <adryeldev@hotmail.com>
2022-12-24 12:36:15 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// variables for navbar menu toggle
+const header=document.querySelector("header"),nav=document.querySelector("nav"),navbarMenuBtn=document.querySelector(".navbar-menu-btn"),navbarForm=document.querySelector(".navbar-form"),navbarSearchBtn=document.querySelector(".navbar-search-btn"),navbarFormCloseBtn=document.querySelector(".navbar-form-close");function navIsActive(){header.classList.toggle("active"),nav.classList.toggle("active"),navbarMenuBtn.classList.toggle("active")}navbarMenuBtn.addEventListener("click",navIsActive);const searchBarIsActive=()=>navbarForm.classList.toggle("active");navbarSearchBtn.addEventListener("click",searchBarIsActive),navbarFormCloseBtn.addEventListener("click",searchBarIsActive);
